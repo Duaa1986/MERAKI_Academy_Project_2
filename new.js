6cf65542e8f685804ede67b303f1df6d699585aa
Duaa1986 <doua.abusair@gmail.com>
2021-04-28 19:50:11 +0300

    creating to do

---
+++ new.js
@@ -0,0 +1,20 @@
+$("#task-list").hide();
+$("#task-work").hide();
+$("#task-others").hide();
+
+const openlist = (e, button) => {
+  if (button === "Personal") {
+    $("#task-list").show();
+    $("#task-work").hide();
+    $("#task-others").hide();
+  } else if (button === "work") {
+    $("#task-list").hide();
+    $("#task-work").show();
+    $("#task-others").hide();
+  }else if (button === "others") {
+  $("#task-list").hide();
+  $("#task-work").hide();
+  $("#task-others").show();
+};
+} ;
+
