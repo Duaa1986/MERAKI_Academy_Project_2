e1c531bf1e6f0c4ca3106f16e103c95160783ff5
Duaa1986 <doua.abusair@gmail.com>
2021-04-29 06:08:09 +0300

    modifying html css

--- new.js
+++ new.js
@@ -1,20 +1,20 @@
 
 // to do list page 
-$("#task-list").hide();
+$("#task-personal").hide();
 $("#task-work").hide();
 $("#task-others").hide();
 
 const openlist = (e, button) => {
   if (button === "Personal") {
-    $("#task-list").show();
+    $("#task-personal").show();
     $("#task-work").hide();
     $("#task-others").hide();
   } else if (button === "work") {
-    $("#task-list").hide();
+    $("#task-personal").hide();
     $("#task-work").show();
     $("#task-others").hide();
   }else if (button === "others") {
-  $("#task-list").hide();
+  $("#task-personal").hide();
   $("#task-work").hide();
   $("#task-others").show();
 };
