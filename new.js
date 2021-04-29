35b155c2d4f83f11e1450bce46d91a1284ae3535
Duaa1986 <doua.abusair@gmail.com>
2021-04-29 06:37:38 +0300

    html js fix

--- new.js
+++ new.js
@@ -1,22 +1,89 @@
 
-// to do list page 
-$("#app").hide();
-$("#app1").hide();
-$("#app2").hide();
+document.getElementById("add-task").addEventListener("click", function () {
+  let taskValue = document.getElementById("task-value").value;
+  if (taskValue) addTask(taskValue);
+  document.getElementById("task-value").value = "";
+});
 
-const openlist = (e, button) => {
-  if (button === "Personal") {
-    $("#app2").show();
-    $("#app1").hide();
-    $("#app").hide();
-  } else if (button === "work") {
-    $("#app2").hide();
-    $("#app1").show();
-    $("#app").hide();
-  }else if (button === "others") {
-  $("#app2").hide();
-  $("#app1").hide();
-  $("#app").show();
+const addTask = (taskValue) => {
+  let task = document.createElement("li");
+  task.classList.add("task");
+  task.classList.add("fill");
+  task.setAttribute("draggable", "true");
+  task.addEventListener("dragstart", dragStart);
+  task.addEventListener("dragend", dragEnd);
+
+  let taskContent = document.createElement("div");
+  taskContent.classList.add("task-content");
+  taskContent.innerText = taskValue;
+
+  let trash = document.createElement("div");
+  trash.classList.add("trash");
+  trash.innerText = "X";
+  trash.addEventListener("click", removeTask);
+
+  task.appendChild(taskContent);
+  task.appendChild(trash);
+
+  let tasks = document.getElementById("tasks-added");
+  tasks.insertBefore(task, tasks.childNodes[0]);
 };
-} ;
 
+const removeTask = (event) => {
+  let tasks = event.target.parentNode.parentNode;
+  let task = event.target.parentNode;
+  tasks.removeChild(task);
+};
+
+// DRAG & DROP
+
+let task;
+
+const dragStart = (event) => {
+  // console.log(event.target);
+  event.target.className += " hold";
+  task = event.target;
+  setTimeout(() => (event.target.className = "invisible"), 0);
+};
+
+const dragEnd = (event) => {
+  // console.log(event.target);
+  event.target.className = "task fill";
+};
+
+const dropzones = document.querySelectorAll(".dropzone");
+
+const dragEnter = (event) => {
+  // console.log("ENTER");
+  event.preventDefault();
+  if (event.target.className === "column dropzone") {
+    event.target.className += " hovered";
+  }
+};
+
+const dragOver = (event) => {
+  // console.log("OVER");
+  event.preventDefault();
+};
+
+const dragLeave = (event) => {
+  // console.log("LEAVE");
+  if (event.target.className === "column dropzone hovered") {
+    event.target.className = "column dropzone";
+  }
+};
+
+const dragDrop = (event) => {
+  // console.log("DROP");
+  if (event.target.className === "column dropzone hovered") {
+    event.target.className = "column dropzone";
+  }
+  event.target.append(task);
+};
+
+for (const dropzone of dropzones) {
+  dropzone.addEventListener("dragenter", dragEnter);
+  dropzone.addEventListener("dragover", dragOver);
+  dropzone.addEventListener("dragleave", dragLeave);
+  dropzone.addEventListener("drop", dragDrop);
+}
