dcf1800eb66ce0ad357cafa2ffeca92ebfbcc76b
Duaa1986 <doua.abusair@gmail.com>
2021-05-01 02:50:05 +0300

    updating html css

--- new.js
+++ new.js
@@ -11,94 +11,75 @@ const openlist = (button) => {
     $("#task-list").hide();
     $("#task-work").show();
     $("#task-others").hide();
-  }else if (button === "others") {
-  $("#task-list").hide();
-  $("#task-work").hide();
-  $("#task-others").show();
+  } else if (button === "others") {
+    $("#task-list").hide();
+    $("#task-work").hide();
+    $("#task-others").show();
+  }
 };
-} ;
 
-
-let personalCount =0;
-let workCount =  0 ;
+let personalCount = 0;
+let workCount = 0;
 let othersCount = 0;
 
 function addtask() {
-  
-
-
-  let value = $('#category1').val();
+  let value = $("#category1").val();
   if (value === "Personal") {
-    
     $("#personalPendeing").append(`<div>
-    <p> ${$('#Addtask').val()} </p>
-    <p> ${$('#date').val()} </p>
+    <p> ${$("#Addtask").val()} </p>
+    <p> ${$("#date").val()} </p>
     </div>
     <button  id='personalcompleted${personalCount}'>Add to completed</button>
     `);
 
-
-    $(`#personalcompleted${personalCount}`).click(addToCompleted)
-
+    $(`#personalcompleted${personalCount}`).click(addToCompleted);
 
     personalCount++;
-    
-   
   } else if (value === "Work") {
     $("#workPendeing").append(`<div>
-    <p> ${$('#Addtask').val()} </p>
-    <p> ${$('#date').val()} </p>
+    <p> ${$("#Addtask").val()} </p>
+    <p> ${$("#date").val()} </p>
     </div>
-    <button  class='workcompleted${workCount}'>Add to completed</button>`
-    );
-    $(`.workcompleted${workCount}`).click(addToCompleted2)
+    <button  class='workcompleted${workCount}'>Add to completed</button>`);
+    $(`.workcompleted${workCount}`).click(addToCompleted2);
 
     workCount++;
-
-  }else if (value === "Others") {
+  } else if (value === "Others") {
     $("#othersPendeing").append(`<div>
-    <p> ${$('#Addtask').val()} </p>
-    <p> ${$('#date').val()} </p>
+    <p> ${$("#Addtask").val()} </p>
+    <p> ${$("#date").val()} </p>
     </div>  
-    <button  class='otherscompleted${othersCount}'>Add to completed</button> `
-    );
-    $(`.otherscompleted${othersCount}`).click(addToCompleted3)
+    <button  class='otherscompleted${othersCount}'>Add to completed</button> `);
+    $(`.otherscompleted${othersCount}`).click(addToCompleted3);
   }
-othersCount++;
+  othersCount++;
 
-  $('#Addtask').val("")
-  $('#Addtask').focus()
-
+  $("#Addtask").val("");
+  $("#Addtask").focus();
 }
-
 
 function addToCompleted(e) {
   e.target.style.display = "none";
-  $('#personalCompleted').append(e.target.previousElementSibling)
-  $('#personalPendeing').remove(`#${e.target.previousElementSibling.id}`)
-  
-  }
- 
+  $("#personalCompleted").append(e.target.previousElementSibling);
+  $("#personalPendeing").remove(`#${e.target.previousElementSibling.id}`);
+}
 
 function addToCompleted2(e) {
   e.target.style.display = "none";
-  $('#workCompleted').append(e.target.previousElementSibling)
-  $('#workPendeing').remove(`#${e.target.previousElementSibling.id}`)
+  $("#workCompleted").append(e.target.previousElementSibling);
+  $("#workPendeing").remove(`#${e.target.previousElementSibling.id}`);
 }
 
 function addToCompleted3(e) {
   e.target.style.display = "none";
-  $('#othersCompleted').append(e.target.previousElementSibling)
-  $('#othersPendeing').remove(`#${e.target.previousElementSibling.id}`)
+  $("#othersCompleted").append(e.target.previousElementSibling);
+  $("#othersPendeing").remove(`#${e.target.previousElementSibling.id}`);
 }
 
-
 function cleartask() {
-  $("#workCompleted").html('<h2>work completed</h2>')
-  $('#personalCompleted').html(' <h2>personal completed</h2>')
-  $('#othersCompleted').html('<h2> others completed</h2>')
-
-
+  $("#workCompleted").html("<h2>work completed</h2>");
+  $("#personalCompleted").html(" <h2>personal completed</h2>");
+  $("#othersCompleted").html("<h2> others completed</h2>");
 }
 
 //var data =[];
@@ -106,11 +87,10 @@ function cleartask() {
 
 //localStorage.setItem('dataInfo', JSON.stringify(data));
 
-
 //var addNewdata = funcation (kay,value) {
-  //var data = JSON.parse(localStorage.getItem('dataInfo')) || [];
-  //data.push({kay: kay ,value : value});
+//var data = JSON.parse(localStorage.getItem('dataInfo')) || [];
+//data.push({kay: kay ,value : value});
 
-  //localStorage.setItem('dataInfo', JSON.stringify(data));
+//localStorage.setItem('dataInfo', JSON.stringify(data));
 
 //}
